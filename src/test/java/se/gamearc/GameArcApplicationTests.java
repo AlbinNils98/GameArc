568f031761e767330415bf1b4260d89ba5e5e74c
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-11-19 16:34:53 +0100

    Initial commit

---
+++ src/test/java/se/gamearc/GameArcApplicationTests.java
@@ -0,0 +1,13 @@
+package se.gamearc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
