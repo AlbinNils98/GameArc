829b2191d027d9c407de90f91cd17a575e581d8d
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2024-12-12 13:39:01 +0100

    Made enpoints for games
    Created enpoints for games. All, byId and byTitle

--- src/main/java/se/gamearc/game/repository/GameRepository.java
+++ src/main/java/se/gamearc/game/repository/GameRepository.java
@@ -3,5 +3,8 @@ package se.gamearc.game.repository;
 import org.springframework.data.repository.ListCrudRepository;
 import se.gamearc.game.entity.Game;
 
+import java.util.Optional;
+
 public interface GameRepository extends ListCrudRepository<Game, Integer> {
+  Optional<Game> findByTitle(String title);
 }
