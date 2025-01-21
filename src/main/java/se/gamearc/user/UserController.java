3e5935264c439aae49890773624b1933d3d52c98
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2025-01-21 22:45:19 +0100

    Refactored userGameController to use userId from session attribute

--- src/main/java/se/gamearc/user/UserController.java
+++ src/main/java/se/gamearc/user/UserController.java
@@ -15,36 +15,14 @@ public class UserController {
 
   private final UserService userService;
 
-  AuthenticationManager authenticationManager;
-
   public UserController(UserService userService, AuthenticationManager authenticationManager) {
     this.userService = userService;
-    this.authenticationManager = authenticationManager;
   }
 
-
-  @PostMapping("/api/register")
+  @PostMapping("/register")
   public ResponseEntity<String> register(@RequestBody UserDto user) throws Exception {
    userService.registerUser(user);
    return ResponseEntity.ok("User created successfully");
-  }
-
-  @PostMapping("/api/login")
-  public ResponseEntity<String> login(@RequestBody UserDto user) throws Exception {
-
-    try {
-      UsernamePasswordAuthenticationToken authToken =
-          new UsernamePasswordAuthenticationToken(user.username(), user.password());
-      Authentication auth = authenticationManager.authenticate(authToken);
-      SecurityContextHolder.getContext().setAuthentication(auth);
-
-      return ResponseEntity.ok("User logged in successfully");
-    }catch (Exception e) {
-      return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-
-
   }
 
 }
